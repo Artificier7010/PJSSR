f2eb7540bc8e34daf23fef989381f751b076ed37
Artificier7010 <83426407+Artificier7010@users.noreply.github.com>
2022-11-29 13:44:25 +0530

    third commit

--- src/Components/carousel/MyCarousel.js
+++ src/Components/carousel/MyCarousel.js
@@ -1,10 +1,10 @@
-import React, { useState } from 'react';
+import React from 'react';
 import "./carousel.scss";
 import "react-responsive-carousel/lib/styles/carousel.min.css";
 import { Carousel } from 'react-responsive-carousel';
-import img1 from '../../Assets/Images/ej-yao-D46mXLsQRJw-unsplash.jpg';
-import img2 from '../../Assets/Images/maksym-kaharlytskyi-u13zBF4r56A-unsplash.jpg';
-import img3 from '../../Assets/Images/thisisengineering-raeng-lx8CooX66ms-unsplash.jpg';
+// import img1 from '../../Assets/Images/ej-yao-D46mXLsQRJw-unsplash.jpg';
+// import img2 from '../../Assets/Images/maksym-kaharlytskyi-u13zBF4r56A-unsplash.jpg';
+// import img3 from '../../Assets/Images/thisisengineering-raeng-lx8CooX66ms-unsplash.jpg';
 
 
 const MyCarousel = () => {
@@ -34,9 +34,9 @@ const MyCarousel = () => {
   }
   return (
     <div className="carousel">
-      <Carousel showIndicators={false} showThumbs={false} interval={5000} autoPlay infiniteLoop swipeable onChange={handleCrouselChange}>
+      <Carousel showStatus={false} showIndicators={false} showThumbs={false} interval={5000} dynamicHeight swipeable={false} autoPlay infiniteLoop onChange={handleCrouselChange}>
         <div className='carousel-wrapper' >
-          <img src={img1} loading="lazy" alt="img1"  />
+          <img src="https://ik.imagekit.io/dexo68yudb/ej-yao-D46mXLsQRJw-unsplash.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1669095729871"  alt="img1"  />
           <div className="header-content" id='first' >
           <div className="headercontent-wrap">
               <h1 className="lighttxt">Inspiring The Next</h1>
@@ -46,7 +46,7 @@ const MyCarousel = () => {
           </div>
         </div>
         <div className='carousel-wrapper'>
-          <img src={img2} loading="lazy" alt="img1"  />
+          <img src="https://ik.imagekit.io/dexo68yudb/maksym-kaharlytskyi-u13zBF4r56A-unsplash.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1669095728328"  alt="img1"  />
           <div className="header-content" id='second'>
             <div className="headercontent-wrap">
               <h1 className="lighttxt">Inspiring The Next</h1>
@@ -56,7 +56,7 @@ const MyCarousel = () => {
           </div>
         </div>
         <div className='carousel-wrapper'>
-          <img src={img3} loading="lazy" alt="img1"  />
+          <img src="https://ik.imagekit.io/dexo68yudb/thisisengineering-raeng-lx8CooX66ms-unsplash.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1669095730733" alt="img1"  />
           <div className="header-content" id='third'>
           <div className="headercontent-wrap">
               <h1 className="lighttxt">Inspiring The Next</h1>
--- src/Components/navbar/Navbar.js
+++ src/Components/navbar/Navbar.js
@@ -1,40 +1,68 @@
-import React, { useState } from 'react';
+import React, {useState } from 'react';
 import './navbar.scss';
 import logo from '../../Assets/Images/logolight.png';
-import { FaHome, FaInfo, FaMailBulk, FaNewspaper, FaSearch, FaServicestack } from 'react-icons/fa';
+import { FaHome, FaInfo, FaMailBulk,FaNewspaper, FaSearch, FaServicestack } from 'react-icons/fa';
+import { HiBars3BottomRight } from 'react-icons/hi2';
+import { GrFormClose } from 'react-icons/gr';
 
 const Navbar = () => {
-    const [isScrolled,setIsScrolled]=useState(false);
+    const [isScrolled, setIsScrolled] = useState(false);
+    const [menuOpened,setMenuOpened]=useState(false);
 
-    window.onscroll=()=>{
-        setIsScrolled(window.pageYOffset > 80 ? true :false);
-        return ()=>(window.onscroll=null);
+
+
+    window.onscroll = () => {
+        setIsScrolled(window.pageYOffset > 80 ? true : false);
+        return () => (window.onscroll = null);
     }
-  return (
-    <div className={isScrolled?"navbar scroll-effect":"navbar"}>
-        <div className="nav-cont">
-            <div className="left-menu">
-                <div className="logo">
-                    <img src={logo} alt="hello" style={{width:"150px"}}/>
-                </div>
-                <div className="search-box">
-                    <input type="text" className='search-input' placeholder='Search Here' />
-                    <button className='search-btn' ><FaSearch/></button>
-                </div>
-            </div>
-            <div className="right-menu">
-                <ul className="nav-items">
-                    <li className="routes"><FaHome className='menu-icon'/><span className='menuitmrev'>HOME</span></li>
-                    <li className="routes"><FaInfo className='menu-icon'/><span className='menuitmrev'>ABOUT US</span></li>
-                    <li className="routes"><FaServicestack className='menu-icon'/><span className='menuitmrev'>SERVICES</span></li>
-                    <li className="routes"><FaNewspaper className='menu-icon'/><span className='menuitmrev'>LATEST NEWS</span></li>
-                    <li className="routes"><FaMailBulk className='menu-icon'/><span className='menuitmrev'>CONTACT</span></li>
-                </ul>
 
-            </div>
-        </div>
-    </div>
-  )
+    const handleMenuBar=(e)=>{
+        setMenuOpened(!menuOpened);
+    }
+    return (
+        <div className={isScrolled ? "navbar scroll-effect" : "navbar"}>
+            <div className="nav-cont">
+                <div className="left-menu">
+                    <div className="logo">
+                        <img src={logo} alt="hello" style={{ width: "150px" }} />
+                    </div>
+
+                    <div className="search-box">
+                        <input type="text" className='search-input' placeholder='Search Here' />
+                        <button className='search-btn' ><FaSearch /></button>
+                    </div>
+
+
+                </div>
+                <div className="right-menu">
+                    {/* NAV ITEMS MOBILE */}
+                    {/* ************************** */}
+                    <div className='nav-item-mobile'>
+                        <button onClick={handleMenuBar} >{!menuOpened ?<HiBars3BottomRight/> : <GrFormClose/> }</button>
+                        <ul className={menuOpened ? isScrolled ? "nav-items-for-mobl openedmenu navscrleffect" :"nav-items-for-mobl openedmenu": "nav-items-for-mobl"}>
+                            <li className="routes-mbl"><FaHome className='menu-icon' /><span className='menuitmrev'>HOME</span></li>
+                            <li className="routes-mbl"><FaInfo className='menu-icon' /><span className='menuitmrev'>ABOUT US</span></li>
+                            <li className="routes-mbl"><FaServicestack className='menu-icon' /><span className='menuitmrev'>SERVICES</span></li>
+                            <li className="routes-mbl"><FaNewspaper className='menu-icon' /><span className='menuitmrev'>LATEST NEWS</span></li>
+                            <li className="routes-mbl"><FaMailBulk className='menu-icon' /><span className='menuitmrev'>CONTACT</span></li>
+                        </ul>
+                    </div>
+                    {/* NAV ITEMS */}
+                    {/* ************************** */}
+                    <ul className="nav-items">
+                        <li className="routes"><FaHome className='menu-icon' /><span className='menuitmrev'>HOME</span></li>
+                        <li className="routes"><FaInfo className='menu-icon' /><span className='menuitmrev'>ABOUT US</span></li>
+                        <li className="routes"><FaServicestack className='menu-icon' /><span className='menuitmrev'>SERVICES</span></li>
+                        <li className="routes"><FaNewspaper className='menu-icon' /><span className='menuitmrev'>LATEST NEWS</span></li>
+                        <li className="routes"><FaMailBulk className='menu-icon' /><span className='menuitmrev'>CONTACT</span></li>
+                    </ul>
+
+
+
+                </div>
+            </div >
+        </div >
+    )
 }
 
 export default Navbar;
